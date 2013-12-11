5650ef5448fb09af38d27b4ab5b2936d9d40c42f
Yuki Fujiwara <sky.y.0079@gmail.com>
2013-12-11 19:41:58 +0900

    Added ODT and HTML

--- markdown-menus-uninstall.bat
+++ markdown-menus-uninstall.bat
@@ -3,7 +3,10 @@
 reg delete "HKLM\SOFTWARE\Classes\*\shell\mkd2doc\command" /f
 reg delete "HKLM\SOFTWARE\Classes\*\shell\mkd2doc" /f
 
-reg delete "HKLM\SOFTWARE\Classes\*\shell\mkd2pdf\command" /f
-reg delete "HKLM\SOFTWARE\Classes\*\shell\mkd2pdf" /f
+reg delete "HKLM\SOFTWARE\Classes\*\shell\mkd2odt\command" /f
+reg delete "HKLM\SOFTWARE\Classes\*\shell\mkd2odt" /f
+
+reg delete "HKLM\SOFTWARE\Classes\*\shell\mkd2html\command" /f
+reg delete "HKLM\SOFTWARE\Classes\*\shell\mkd2html" /f
 
 exit
--- markdown-menus-win8.bat
+++ markdown-menus-win8.bat
@@ -3,7 +3,10 @@
 reg add "HKLM\SOFTWARE\Classes\*\shell\mkd2doc" /ve /d "Markdown to Word"
 reg add "HKLM\SOFTWARE\Classes\*\shell\mkd2doc\command" /ve /d "%USERPROFILE%\AppData\Local\Pandoc\pandoc.exe -s -S \"%%1\" -o \"%%1.docx\""
 
-reg add "HKLM\SOFTWARE\Classes\*\shell\mkd2pdf" /ve /d "Markdown to PDF"
-reg add "HKLM\SOFTWARE\Classes\*\shell\mkd2pdf\command" /ve /d "%USERPROFILE%\AppData\Local\Pandoc\pandoc.exe \"%%1\" -o \"%%1.pdf\" -V documentclass=ltjarticle --latex-engine=lualatex"
+reg add "HKLM\SOFTWARE\Classes\*\shell\mkd2odt" /ve /d "Markdown to ODT"
+reg add "HKLM\SOFTWARE\Classes\*\shell\mkd2odt\command" /ve /d "%USERPROFILE%\AppData\Local\Pandoc\pandoc.exe -s -S \"%%1\" -o \"%%1.odt\""
+
+reg add "HKLM\SOFTWARE\Classes\*\shell\mkd2html" /ve /d "Markdown to HTML"
+reg add "HKLM\SOFTWARE\Classes\*\shell\mkd2html\command" /ve /d "%USERPROFILE%\AppData\Local\Pandoc\pandoc.exe \"%%1\" -s -o \"%%1.html\""
 
 exit
